a7d20bd27ec0e19e16e6902a676a5935fc03c310
alinaaksanova <aksyanova.alina@gmail.com>
2025-03-19 23:52:40 +0300

    отредачила задачу 8

--- lab2/task8/src/task8.py
+++ lab2/task8/src/task8.py
@@ -6,8 +6,9 @@ PATH = os.path.dirname(os.path.abspath(__file__))
 
 def find_tree_height(tree, root):
     """Рекурсивный поиск высоты дерева"""
-    if root == 0:
+    if root == 0: #если узел отсутствует то высота дерева = 0
         return 0
+    # в обратном случае находим высоту поддеревьев и возвращаем максимум + 1 (корень)
     left = tree[root][1]
     right = tree[root][2]
     return 1 + max(find_tree_height(tree, left), find_tree_height(tree, right))
@@ -15,25 +16,27 @@ def find_tree_height(tree, root):
 
 def main():
     # Читаем данные из файла
-    lst = utils.read_file(PATH)
-    N = int(lst[0])  # Количество вершин
+    lst = utils.read_file(PATH) #строки
+    N = int(lst[0])  #количество вершин
 
     if N == 0:
         utils.write_file(PATH, ["0"])
         return
 
+    # строим дерево
+
     tree = {}
-    children = set()
+    children = set() #дочерние узлы
 
     for i in range(1, N + 1):
-        key, left, right = map(int, lst[i].split())
+        key, left, right = map(int, lst[i].split()) #узел и потомки
         tree[i] = (key, left, right)
         if left > 0:
             children.add(left)
         if right > 0:
             children.add(right)
 
-    # Определяем корень (он не встречается в списке детей)
+    # Определяем корень, вычитая все дочерние узлы, оставляя только корень
     root = (set(range(1, N + 1)) - children).pop()
 
     # Вычисляем высоту дерева
